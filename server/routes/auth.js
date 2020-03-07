7c7d9fcfb68152757dc57c14c8b44f13ff70a24f
saltanat@pingocean.com <saltanat@pingocean.com>
2020-03-07 22:51:30 +0600

    signin and signup with using email verification

---
+++ server/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+
+const { signup, accountActivation, signin } = require('../controllers/auth')
+const { userSignupValidator,userSigninValidator } = require('../validators/auth')
+const { runValidation } = require('../validators')
+
+
+router.post('/signup', userSignupValidator, runValidation, signup);
+router.post('/account-activation', accountActivation)
+router.post('/signin', userSigninValidator, runValidation, signin)
+module.exports = router
